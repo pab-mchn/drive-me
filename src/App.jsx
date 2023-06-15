6f2acf4b719e03962290c9b8cb968912e99cf6dd
cg-diazborromeo <Gastadiaz@gmail.com>
2023-06-15 20:27:00 +0200

    feat: Introduce new features to the landing page
    Starting to develop the sections to download the app and the step by step to use it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar/Navbar";
 import SliderComponent from "./components/Slider/Slider";
 import AboutUs from "./components/About/AboutUs";
+import Booking from "./components/Booking/Booking";
+import StepByStep from "./components/StepByStep/StepByStep";
+
 function App() {
   return (
     <>
       <Navbar />
       <SliderComponent />
       <AboutUs />
+      <Booking />
+      <StepByStep />
     </>
   );
 }
---
+++ src/components/StepByStep/StepByStep.jsx
@@ -0,0 +1,47 @@
+import './StepByStep.css';
+
+const StepByStep = () => {
+  return (
+    <div className='main-container'>
+        <h2>Explore the routes to Ireland!</h2>
+        <div className='content'>
+            <div className='step-container'>
+                <div className='image'>
+                    <img src='src\assets\map-icon.png' alt='About Us' />
+                </div>
+                <div className='title-container'>
+                    <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                </div>
+                <div className='text-container'>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore voluptatum blanditiis dolore! Eaque, dolor minima ex voluptate eius esse error?</p>
+                </div>
+            </div>
+            <div className='step-container'>
+                <div className='image'>
+                  <img src='src\assets\map-icon.png' alt='About Us' />
+                </div>
+                <div className='title-container'>
+                  <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                </div>
+                <div className='text-container'>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore voluptatum blanditiis dolore! Eaque, dolor minima ex voluptate eius esse error?</p>
+                </div>
+            </div>
+            <div className='step-container'>
+                <div className='image'>
+                    <img src='src\assets\map-icon.png' alt='About Us' />
+                </div>
+                <div className='title-container'>
+                    <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                </div>
+                <div className='text-container'>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore voluptatum blanditiis dolore! Eaque, dolor minima ex voluptate eius esse error?</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default StepByStep
