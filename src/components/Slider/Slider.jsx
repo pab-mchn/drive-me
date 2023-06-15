c8de8b3fe52346516bc54f2e42b8ece2cfa6ba02
pab-mich <pab.michelini@gmail.com>
2023-06-15 12:16:46 +0100

    slider home functionality and basic design

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,7 +7,10 @@ import "./Slider.css";
 const Slide = ({ image, title }) => (
   <div className='slide'>
     <img src={image} alt={title} />
-    <h3>{title}</h3>
+    <div className='slide-content'>
+      <h1>Your Trip, Your Adventure</h1>
+      <button className='download-button'>Get the App</button>
+    </div>
   </div>
 );
 
@@ -20,24 +23,29 @@ const SliderComponent = () => {
     slidesToScroll: 1,
     autoplay: true, // autoplay
     autoplaySpeed: 2000, // Intervalo de tiempo entre cada slide (en milisegundos)
+    appendDots: (dots) => (
+      <div style={{ position: "absolute", bottom: 10, width: "100%", textAlign: "center" }}>
+        <ul style={{ margin: "0", padding: "0" }}>{dots}</ul>
+      </div>
+    ),
+    customPaging: () => (
+      <div style={{ width: "10px", height: "10px", borderRadius: "50%", background: "#fff", margin: "0 5px" }}></div>
+    ),
   };
 
   const slides = [
     {
       id: 1,
       image: "https://images.pexels.com/photos/12194164/pexels-photo-12194164.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Slide 1",
     },
     {
       id: 2,
       image:
         "https://images.pexels.com/photos/620335/pexels-photo-620335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      title: "Slide 2",
     },
     {
       id: 3,
       image: "https://images.pexels.com/photos/2061417/pexels-photo-2061417.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Slide 3",
     },
   ];
 
@@ -45,7 +53,9 @@ const SliderComponent = () => {
     <div className='slider-wrapper'>
       <Slider {...settings}>
         {slides.map((slide) => (
-          <Slide key={slide.id} image={slide.image} title={slide.title} />
+          <div key={slide.id}>
+            <Slide image={slide.image} />
+          </div>
         ))}
       </Slider>
     </div>
