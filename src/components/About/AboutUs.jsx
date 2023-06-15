f483b99e62c0550ee37b4b57a29fa79d26523fbc
pab-mich <pab.michelini@gmail.com>
2023-06-15 17:14:43 +0100

    included firstprogress in  ui and functionality in aboutUs

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -1,12 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./AboutUs.css";
 
 const AboutUs = () => {
   const [selectedButton, setSelectedButton] = useState(1);
+  const [isLoopActive, setIsLoopActive] = useState(true);
 
   const handleButtonClick = (buttonId) => {
     setSelectedButton(buttonId);
+    setIsLoopActive(false); // Detener el loop cuando el usuario selecciona un botón
   };
+
+  useEffect(() => {
+    let intervalId = null;
+
+    if (isLoopActive) {
+      intervalId = setInterval(() => {
+        setSelectedButton((prevButton) => {
+          // Incrementa el valor del botón actual en 1
+          return prevButton === 3 ? 1 : prevButton + 1;
+        });
+      }, 10000); // Cambia de botón cada 10 segundos
+    }
+
+    return () => {
+      // Limpia el intervalo cuando el componente se desmonta
+      clearInterval(intervalId);
+    };
+  }, [isLoopActive]);
 
   return (
     <div className='container'>
@@ -32,35 +52,52 @@ const AboutUs = () => {
         </button>
       </div>
 
-      {selectedButton === 1 && (
-        <div className='content'>
-          <p>
-            We are Drive Me, an innovative platform that connects drivers and passengers to facilitate safe and
-            convenient ridesharing. We have a mission to transform the way people travel, providing an efficient and
-            collaborative alternative.
-          </p>
-        </div>
-      )}
-      {selectedButton === 2 && (
-        <div className='content'>
-          <p>
-            We offer a simple solution for drivers who regularly travel in their own vehicles. Our application allows
-            drivers to register their trips and offer available seats to passengers who share the same route. This
-            creates an opportunity to share expenses and reduce traffic congestion while also encouraging social
-            interaction and community.
-          </p>
-        </div>
-      )}
-      {selectedButton === 3 && (
-        <div className='content'>
-          <p>
-            Because we believe in collaborative and sustainable travel. Our platform offers affordable, safe, and
-            verified ridesharing options. Join our community for convenient and eco-friendly commuting, enhanced by user
-            ratings and feedback. Simplify your daily travels, connect with others, and contribute to a better future
-            with Drive Me.
-          </p>
-        </div>
-      )}
+      <div className='content-container'>
+        {selectedButton === 1 && (
+          <div className='content'>
+            <div className='image'>
+              <img src='https://i.pinimg.com/564x/74/56/d1/7456d1abee19dd95d6e68dee0521e4b4.jpg' alt='About Us' />
+            </div>
+            <div className='text'>
+              <p>
+                We are Drive Me, an innovative platform that connects drivers and passengers to facilitate safe and
+                convenient ridesharing. We have a mission to transform the way people travel, providing an efficient and
+                collaborative alternative.
+              </p>
+            </div>
+          </div>
+        )}
+        {selectedButton === 2 && (
+          <div className='content'>
+            <div className='image'>
+              <img src='https://i.pinimg.com/564x/74/56/d1/7456d1abee19dd95d6e68dee0521e4b4.jpg' alt='About Us' />
+            </div>
+            <div className='text'>
+              <p>
+                We offer a simple solution for drivers who regularly travel in their own vehicles. Our application
+                allows drivers to register their trips and offer available seats to passengers who share the same route.
+                This creates an opportunity to share expenses and reduce traffic congestion while also encouraging
+                social interaction and community.
+              </p>
+            </div>
+          </div>
+        )}
+        {selectedButton === 3 && (
+          <div className='content'>
+            <div className='image'>
+              <img src='https://i.pinimg.com/564x/74/56/d1/7456d1abee19dd95d6e68dee0521e4b4.jpg' alt='About Us' />
+            </div>
+            <div className='text'>
+              <p>
+                Because we believe in collaborative and sustainable travel. Our platform offers affordable, safe, and
+                verified ridesharing options. Join our community for convenient and eco-friendly commuting, enhanced by
+                user ratings and feedback. Simplify your daily travels, connect with others, and contribute to a better
+                future with Drive Me.
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
